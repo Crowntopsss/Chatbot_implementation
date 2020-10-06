637f62c0ffd2f18b76891c1732ec3258ab22c1be
Crowntopsss <adebiyitope26@gmail.com>
2020-10-06 13:46:51 +0100

    training

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,7 @@
 import nltk
-nltk.download('punkt')
+import numpy as np
+
+
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
@@ -10,7 +12,14 @@ def tokennize(sentence):
 def stem(word):
     return stemmer.stem(word.lower())
 
-
-
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    tokenized_sentence = [stem(w) for w tokenized_sentence]
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
+
+sentence = ["hello", "how", "are", "you"]
+words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+bag = bag_of_words(sentence, words)
--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 import json
 from nltk_utils import tokennize, stem, bag_of_words
+import numpy as np
+import torch 
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
 with open('intents.json') as f:
     intents =  json.load(f)
@@ -17,4 +21,30 @@ for intent in intents['intents']:
         xy.append((w, tag))
 
 ignore_words = ['?', '!', '.', ','] 
+all_words =  [stem(w) for w in all_words if w not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
+
 print(all_words)
+
+X_train = []
+y_train = []
+
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    y_train.append(label)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    def __getitem__(self, index):
+        
